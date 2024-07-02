c631d3957da8c831e467069de5630b7b7fda15f0
letpwk <leticiass.pw@gmail.com>
2024-07-02 11:07:10 -0400

    olhar o "ATENÇÃO"!

--- lib/telas/criar_conta.dart
+++ lib/telas/criar_conta.dart
@@ -12,134 +12,161 @@ class _CadastroState extends State<Cadastro> {
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
   bool _isObscure = true;
+  final _formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        decoration: BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage("lib/imagens/fundo.png"),
-            fit: BoxFit.cover,
+    return Form(
+      key: _formKey,
+      child: Scaffold(
+        body: Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage("lib/imagens/fundo.png"),
+              fit: BoxFit.cover,
+            ),
           ),
-        ),
-        padding: const EdgeInsets.only(
-          top: 60,
-          left: 40,
-          right: 40,
-        ),
-        child: 
-            Column(
-              children: [
-                Center(
-                  child: SizedBox(
-                    width: 250,
-                    height: 250,
-                    child: Image.asset('lib/imagens/iconepessoa.png'),
-                  ),
-                ),
-              
-            const SizedBox(height: 50),
-            TextFormField(
-              controller: _usernameController,
-              decoration: InputDecoration(
-                labelText: 'Usuário',
-                labelStyle: TextStyle(color: Colors.white),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10.0),
-                  borderSide: BorderSide.none,
-                ),
-                enabledBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10.0),
-                  borderSide: BorderSide(color: Colors.white, width: 2.0),
-                ),
-                focusedBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10.0),
-                  borderSide: BorderSide(color: Colors.white, width: 2.0),
+          padding: const EdgeInsets.only(
+            top: 60,
+            left: 40,
+            right: 40,
+          ),
+          child: Column(
+            children: [
+              Center(
+                child: SizedBox(
+                  width: 250,
+                  height: 250,
+                  child: Image.asset('lib/imagens/iconepessoa.png'),
                 ),
               ),
-              style: TextStyle(color: Colors.white),
-            ),
-
-            const SizedBox(height: 20),
-            TextFormField(
-              controller: _emailController,
-              decoration: InputDecoration(
-                labelText: 'Email',
-                labelStyle: TextStyle(color: Colors.white),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10.0),
-                  borderSide: BorderSide.none,
-                ),
-                enabledBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10.0),
-                  borderSide: BorderSide(color: Colors.white, width: 2.0),
-                ),
-                focusedBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10.0),
-                  borderSide: BorderSide(color: Colors.white, width: 2.0),
-                ),
-              ),
-              style: TextStyle(color: Colors.white),
-            ),
-
-            const SizedBox(height: 20),
-            TextFormField(
-              controller: _passwordController,
-              obscureText: _isObscure,
-              decoration: InputDecoration(
-                labelText: 'Senha',
-                labelStyle: TextStyle(color: Colors.white),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10.0),
-                  borderSide: BorderSide.none,
-                ),
-                enabledBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10.0),
-                  borderSide: BorderSide(color: Colors.white, width: 2.0),
-                ),
-                focusedBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10.0),
-                  borderSide: BorderSide(color: Colors.white, width: 2.0),
-                ),
-                suffixIcon: IconButton(
-                  icon: Icon(
-                    _isObscure ? Icons.visibility : Icons.visibility_off,
-                    color: Colors.white,
+              const SizedBox(height: 50),
+              TextFormField(
+                validator: (String? value) {
+                  if (value != null && value.isEmpty) {
+                    return "Insira um nome de usuário";
+                  }
+                  return null;
+                },
+                controller: _usernameController,
+                decoration: InputDecoration(
+                  labelText: 'Usuário',
+                  labelStyle: TextStyle(color: Colors.white),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                    borderSide: BorderSide.none,
                   ),
-                  onPressed: () {
-                    setState(() {
-                      _isObscure = !_isObscure;
-                    });
-                  },
-                ),
-              ),
-              style: TextStyle(color: Colors.white),
-            ),
-            const SizedBox(height: 50),
-            SizedBox(
-              width: 250, 
-              height: 50, 
-              child: OutlinedButton(
-                style: OutlinedButton.styleFrom(
-                  side: const BorderSide(
-                    color: Colors.white,
-                    width: 3,
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                    borderSide: BorderSide(color: Colors.white, width: 2.0),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                    borderSide: BorderSide(color: Colors.white, width: 2.0),
                   ),
                 ),
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => const Cadastro()),
-                  );
-                },child: const Text(
-                  'Cadastrar',
-                  style: TextStyle(fontSize: 20, color: Colors.white),
-                ),))
-           
-              
-            
-          ],
+                style: TextStyle(color: Colors.white),
+              ),
+              const SizedBox(height: 20),
+              TextFormField(
+                validator: (String? value) {
+                  if (value != null && value.isEmpty) {
+                    return "Insira um endereço de email";
+                  }
+                  return null;
+                },
+                controller: _emailController,
+                decoration: InputDecoration(
+                  labelText: 'Email',
+                  labelStyle: TextStyle(color: Colors.white),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                    borderSide: BorderSide.none,
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                    borderSide: BorderSide(color: Colors.white, width: 2.0),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                    borderSide: BorderSide(color: Colors.white, width: 2.0),
+                  ),
+                ),
+                style: TextStyle(color: Colors.white),
+              ),
+              const SizedBox(height: 20),
+              TextFormField(
+                validator: (String? value) {
+                  if (value != null && value.isEmpty) {
+                    return "Insira uma senha";
+                  } else {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => const Cadastro()),
+                    ); //ATENÇÃO: ajeitar depois!
+                  };
+                },
+                controller: _passwordController,
+                obscureText: _isObscure,
+                decoration: InputDecoration(
+                  labelText: 'Senha',
+                  labelStyle: TextStyle(color: Colors.white),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                    borderSide: BorderSide.none,
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                    borderSide: BorderSide(color: Colors.white, width: 2.0),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                    borderSide: BorderSide(color: Colors.white, width: 2.0),
+                  ),
+                  suffixIcon: IconButton(
+                    icon: Icon(
+                      _isObscure ? Icons.visibility : Icons.visibility_off,
+                      color: Colors.white,
+                    ),
+                    onPressed: () {
+                      setState(() {
+                        _isObscure = !_isObscure;
+                      });
+                    },
+                  ),
+                ),
+                style: TextStyle(color: Colors.white),
+              ),
+              const SizedBox(height: 50),
+              SizedBox(
+                  width: 250,
+                  height: 50,
+                  child: OutlinedButton(
+                    style: OutlinedButton.styleFrom(
+                      side: const BorderSide(
+                        color: Colors.white,
+                        width: 3,
+                      ),
+                    ),
+                    onPressed: () {
+                      if (_formKey.currentState!.validate()) {
+                        print(_usernameController.text);
+                        print(_emailController.text);
+                        print(_passwordController.text);
+                      }
+                      /*Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const Cadastro()),
+                      );*/
+                    },
+                    child: const Text(
+                      'Cadastrar',
+                      style: TextStyle(fontSize: 20, color: Colors.white),
+                    ),
+                  ))
+            ],
+          ),
         ),
       ),
     );
--- lib/telas/login.dart
+++ lib/telas/login.dart
@@ -13,121 +13,148 @@ class _LoginState extends State<Login> {
   final TextEditingController _usernameController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
   bool _isObscure = true;
+  final _formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        decoration: BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage("lib/imagens/fundo.png"),
-            fit: BoxFit.cover,
+    return Form(
+      key: _formKey,
+      child: Scaffold(
+        body: Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage("lib/imagens/fundo.png"),
+              fit: BoxFit.cover,
+            ),
           ),
-        ),
-        padding: const EdgeInsets.only(
-          top: 60,
-          left: 40,
-          right: 40,
-        ),
-        child: 
-            Column(
-              children: [
-                Center(
-                  child: SizedBox(
-                    width: 250,
-                    height: 250,
-                    child: Image.asset('lib/imagens/icone composteira.png'),
-                  ),
-                ),
-               
-            const SizedBox(height: 50),
-            TextFormField(
-              controller: _usernameController,
-              decoration: InputDecoration(
-                labelText: 'Usuário',
-                labelStyle: TextStyle(color: Colors.white),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10.0),
-                  borderSide: BorderSide.none,
-                ),
-                enabledBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10.0),
-                  borderSide: BorderSide(color: Colors.white, width: 2.0),
-                ),
-                focusedBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10.0),
-                  borderSide: BorderSide(color: Colors.white, width: 2.0),
+          padding: const EdgeInsets.only(
+            top: 60,
+            left: 40,
+            right: 40,
+          ),
+          child: Column(
+            children: [
+              Center(
+                child: SizedBox(
+                  width: 250,
+                  height: 250,
+                  child: Image.asset('lib/imagens/icone composteira.png'),
                 ),
               ),
-              style: TextStyle(color: Colors.white),
-            ),
-  
-            const SizedBox(height: 20),
-            TextFormField(
-              controller: _passwordController,
-              obscureText: _isObscure,
-              decoration: InputDecoration(
-                labelText: 'Senha',
-                labelStyle: TextStyle(color: Colors.white),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10.0),
-                  borderSide: BorderSide.none,
+              const SizedBox(height: 50),
+              TextFormField(
+                validator: (String? value) {
+                  if (value != null && value.isEmpty) {
+                    return "Insira seu nome de usuário";
+                  }
+                  return null;
+                },
+                controller: _usernameController,
+                decoration: InputDecoration(
+                  labelText: 'Usuário',
+                  labelStyle: TextStyle(color: Colors.white),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                    borderSide: BorderSide.none,
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                    borderSide: BorderSide(color: Colors.white, width: 2.0),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                    borderSide: BorderSide(color: Colors.white, width: 2.0),
+                  ),
                 ),
-                enabledBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10.0),
-                  borderSide: BorderSide(color: Colors.white, width: 2.0),
+                style: TextStyle(color: Colors.white),
+              ),
+              const SizedBox(height: 20),
+              TextFormField(
+                validator: (String? value) {
+                  if (value != null && value.isEmpty) {
+                    return "Insira sua senha";
+                  } 
+                  else {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => const Login()),
+                    ); //ATENÇÃO: ajeitar depois!
+                  };
+                },
+                controller: _passwordController,
+                obscureText: _isObscure,
+                decoration: InputDecoration(
+                  labelText: 'Senha',
+                  labelStyle: TextStyle(color: Colors.white),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                    borderSide: BorderSide.none,
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                    borderSide: BorderSide(color: Colors.white, width: 2.0),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                    borderSide: BorderSide(color: Colors.white, width: 2.0),
+                  ),
+                  suffixIcon: IconButton(
+                    icon: Icon(
+                      _isObscure ? Icons.visibility : Icons.visibility_off,
+                      color: Colors.white,
+                    ),
+                    onPressed: () {
+                      setState(() {
+                        _isObscure = !_isObscure;
+                      });
+                    },
+                  ),
                 ),
-                focusedBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10.0),
-                  borderSide: BorderSide(color: Colors.white, width: 2.0),
-                ),
-                suffixIcon: IconButton(
-                  icon: Icon(
-                    _isObscure ? Icons.visibility : Icons.visibility_off,
-                    color: Colors.white,
+                style: TextStyle(color: Colors.white),
+              ),
+              const SizedBox(height: 30),
+              SizedBox(
+                width: 250,
+                height: 50,
+                child: OutlinedButton(
+                  style: OutlinedButton.styleFrom(
+                    side: const BorderSide(
+                      color: Colors.white,
+                      width: 3,
+                    ),
                   ),
                   onPressed: () {
-                    setState(() {
-                      _isObscure = !_isObscure;
-                    });
+                    if (_formKey.currentState!.validate()) {
+                      print(_usernameController.text);
+                      print(_passwordController.text);
+                    }
+                    /*Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => const Login()),
+                    );*/
                   },
-                ),
-              ),
-              style: TextStyle(color: Colors.white),
-            ),
-            const SizedBox(height: 30),
-            
-            SizedBox(
-              width: 250,
-              height: 50, 
-              child: OutlinedButton(
-                style: OutlinedButton.styleFrom(
-                  side: const BorderSide(
-                    color: Colors.white,
-                    width: 3,
+                  child: const Text(
+                    'Entrar',
+                    style: TextStyle(fontSize: 20, color: Colors.white),
                   ),
                 ),
+              ),
+              const Spacer(),
+              TextButton(
                 onPressed: () {
                   Navigator.push(
                     context,
-                    MaterialPageRoute(builder: (context) => const Login()),
-                  );
-                },child: const Text(
-                  'Entrar',
-                  style: TextStyle(fontSize: 20, color: Colors.white),
-                ),),),
-            const Spacer(), 
-            TextButton(onPressed: () {
-              Navigator.push(
-                    context,
                     MaterialPageRoute(builder: (context) => const Cadastro()),
-              );
-
-            },
-            child: const Text('Cadastrar', style: TextStyle(color: Colors.white, fontSize: 20),),
-            ),
-            const SizedBox(height: 20),
-          ],
+                  );
+                },
+                child: const Text(
+                  'Cadastrar',
+                  style: TextStyle(color: Colors.white, fontSize: 20),
+                ),
+              ),
+              const SizedBox(height: 20),
+            ],
+          ),
         ),
       ),
     );
