941ae75dd7f0136a2d26c6ec3192202e65a5aec1
letpwk <leticiass.pw@gmail.com>
2024-09-17 11:55:35 -0400

    tela habitos finalizada

--- lib/telas/habitos.dart
+++ lib/telas/habitos.dart
@@ -61,63 +61,99 @@ class Habitos extends StatelessWidget {
                     ),
                   ),
                   const SizedBox(height: 45),
-                  Row(
-                    crossAxisAlignment: CrossAxisAlignment.start,
+                  Column(
                     children: [
-                      Padding(
-                        padding: const EdgeInsets.only(top: 10.0),
-                        child: Container(
-                          height: 120,
-                          width: 120,
-                          decoration: const BoxDecoration(
-                            image: DecorationImage(
-                              image: AssetImage("lib/imagens/gota.jpg"),
-                              fit: BoxFit
-                                  .cover,
-                            ),
-                          ),
-                        ),
-                      ),
-                      SizedBox(width: 10), 
-                      Expanded(
-                        child: Padding(
-                          padding: const EdgeInsets.only(bottom: 200.0),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start, 
-                            children: [
-                              Text(
-                                'Reaproveite água',
-                                style: TextStyle(
-                                  fontSize: 20,
-                                  color: Colors.green.shade100,
-                                ),
-                              ),
-                              SizedBox(height: 5),
-                              
-                              Container(
-                                 constraints: BoxConstraints(maxWidth: 400)
-                              ,
-                                child: Text(
-                                  'A água da máquina de lavar pode ser reutilizada para a lavagem de calçadas, dar descarga e pequenas limpezas.',
-                                  style: TextStyle(
-                                    fontSize: 18,
-                                    color: Colors.white,
-                                  ),
-                                  textAlign: TextAlign.justify, 
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                      ),
+                      Dicas("lib/imagens/gota.jpg", 'Reaproveite água',
+                          'A água da máquina de lavar pode ser usada novamente para limpar calçadas, dar descarga e limpezas menores.'),
+                      Dicas("lib/imagens/torneira.png", 'Atenção à torneira',
+                          'Enquanto lavar as mãos, escovar os dentes ou lavar o rosto, mantenha a torneira fechada.'),
+                      Dicas(
+                          "lib/imagens/chuveiro.jpg",
+                          'Evite usar chuveiro elétrico',
+                          'O chuveiro elétrico é um dos que mais consome energia, quando estiver frio deixe na posição verão.'),
+                      Dicas('lib/imagens/lampada.jpg', 'Use lâmpadas LEDS',
+                          'A lâmpada LED além de durar mais, são 65% mais econômicas que as fluorescentes.'),
+                      Dicas('lib/imagens/sacola.jpg', 'Use sacolas retornáveis',
+                          'Quando for ao mercado, leve uma sacola reutilizável para evitar o uso de sacolas plásticas.'),
+                      Dicas(
+                          'lib/imagens/geladeira.jpg',
+                          'Evite abrir a geladeira',
+                          'Deixar a geladeira aberta ou abri-la muito consome energia e pode estragar os alimentos.'),
+                      Dicas(
+                          'lib/imagens/janela.jpg',
+                          'Aproveite a claridade do ambiente',
+                          'Desligue as luzes e abra a janela durante o dia. As lâmpadas pode aumentar muito a conta.'),
                     ],
-                  )
+                  ),
                 ],
               ),
             ],
           ),
         ),
       ),
+    );
+  }
+}
+
+class Dicas extends StatefulWidget {
+  final String foto;
+  final String titulo;
+  final String texto;
+  const Dicas(this.foto, this.titulo, this.texto, {Key? key}) : super(key: key);
+
+  @override
+  State<Dicas> createState() => _DicasState();
+}
+
+class _DicasState extends State<Dicas> {
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: const EdgeInsets.only(top: 10.0),
+          child: Container(
+            height: 120,
+            width: 120,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage(widget.foto),
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+        ),
+        SizedBox(width: 10),
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.only(bottom: 50.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  widget.titulo,
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.green.shade100,
+                  ),
+                ),
+                SizedBox(height: 5),
+                Container(
+                  constraints: BoxConstraints(maxWidth: 400),
+                  child: Text(
+                    widget.texto,
+                    style: TextStyle(
+                      fontSize: 18,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
