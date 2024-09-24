f4db1b6db2ae30e7e24154ee3c3487b997f46b5a
letpwk <leticiass.pw@gmail.com>
2024-09-24 11:37:44 -0400

    atualizando

--- lib/telas/comp_terra.dart
+++ lib/telas/comp_terra.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:app/telas/telainicial.dart';
+import 'package:app/telas/criar_conta.dart';
 
 class Comp_terra extends StatelessWidget {
-  final dropValue = ValueNotifier("");
-  final dropOpcoes = ["Terra", "Composteira"];
   Comp_terra({super.key});
 
   @override
@@ -49,9 +48,120 @@ class Comp_terra extends StatelessWidget {
                         ),
                       ],
                     ),
+                    const SizedBox(height: 30),
+                    SizedBox(
+                      width: 200,
+                      height: 40,
+                      child: ElevatedButton(
+                        style: OutlinedButton.styleFrom(
+                          side: const BorderSide(
+                            color: Colors.white,
+                            width: 3,
+                          ),
+                        ),
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => Comp_terra()),
+                          );
+                        },
+                        child: const Text(
+                          'Terra',
+                          style: TextStyle(fontSize: 20, color: Colors.grey),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 10),
+                    SizedBox(
+                      width: 200,
+                      height: 40,
+                      child: OutlinedButton(
+                        style: OutlinedButton.styleFrom(
+                          side: const BorderSide(
+                            color: Colors.white,
+                            width: 3,
+                          ),
+                        ),
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => const Telainicial()),
+                          );
+                        },
+                        child: const Text(
+                          'Composteira',
+                          style: TextStyle(fontSize: 20, color: Colors.white),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 30),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          'Materiais Necessários: ',
+                          style: TextStyle(fontSize: 20, color: Colors.white),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 30),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Pá pequena ',
+                          style: TextStyle(
+                              fontSize: 18, color: Colors.green.shade100),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 10),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Pá pequena de plástico ou metal.',
+                          style: TextStyle(fontSize: 18, color: Colors.white),
+                        ),
+                      ],
+                    ),
+                    Container(
+                      height: 120,
+                      width: double.infinity,
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                          image: AssetImage('lib/imagens/pa.png'),
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 30),
                     Row(
                       children: [
-                        
+                        Column(
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.only(bottom: 65.0),
+                              child: Container(
+                                height: 30,
+                                width: 30,
+                                decoration: BoxDecoration(
+                                  image: DecorationImage(
+                                    image: AssetImage('lib/imagens/lamp2.png'),
+                                    fit: BoxFit.cover,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                        const SizedBox(width: 10),
+                        Text(
+                          'Se não tiver a pá, pode construir\numa usando a embalagem de \namaciante ou de água sanitária\ncom alça.',
+                          style: TextStyle(fontSize: 18, color: Colors.white),
+                        ),
                       ],
                     ),
                   ])
